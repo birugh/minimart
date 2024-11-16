e4376a3b0b1d4840db7f3b3d5b1464fa3f8d0f0d
birugh <notblue.https@gmail.com>
2024-11-16 15:12:27 +0700

    prototype main menu & settings

---
+++ settings.gd
@@ -0,0 +1,25 @@
+extends Control
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value/5)
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(0,toggled_on)
+
+
+func _on_resolutions_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1366, 768))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
