f0d74fdb157edf1f3259c8c92eba132c14d166e6
birugh <notblue.https@gmail.com>
2024-11-17 17:06:48 +0700

    merged & fix some issue

---
+++ Scripts/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var syncPos = Vector2(0,0)
+
+func _ready() -> void:
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
+
+func _physics_process(delta: float) -> void:
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		syncPos = global_position
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		move_and_slide()
+	else:
+		global_position = global_position.lerp(syncPos, .5)
