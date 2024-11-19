5e3f9ee3856fa763f1653c08482353cad4d8cd3d
Neyrrrs <neyrrrs@gmail.com>
2024-11-19 20:27:51 +0700

    Cleaner done

--- Job-action/job-scripts/dirt.gd
+++ Job-action/job-scripts/dirt.gd
@@ -1,11 +1,18 @@
 extends Area2D
 
 var bodyMasuk = false
+var bersihkanDone = false
+
+func _process(delta: float) -> void:
+	if bersihkanDone:
+		queue_free()
 
 func _on_body_entered(body: CharacterBody2D) -> void:
-	print("Body memasuki noda")
 	if body.is_in_group("Player"):
-		Dirt.bodyMasuk = true
+		body.on_enter_dirt(self)  # Panggil fungsi di Player
+		bodyMasuk = true
 
 func _on_body_exited(body: CharacterBody2D) -> void:
-	Dirt.bodyMasuk = false
+	if body.is_in_group("Player"):
+		body.on_exit_dirt(self)  # Panggil fungsi di Player
+		bodyMasuk = false
--- Job-action/job-scripts/player.gd
+++ Job-action/job-scripts/player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D
 
 var SPEED = 0
+var bersihkanDur = 50  # Durasi pembersihan (angka ini bisa diatur sesuai kebutuhan)
+var bersihkan = 0  # Progres pembersihan
+var is_cleaning = false  # Status apakah sedang dalam proses pembersihan
+var current_dirt = null  # Referensi ke instance Dirt yang aktif
+
 func _process(delta: float) -> void:
-	var bersihkanDur = 50
-	var bersihkan = 0
 	var direction = Vector2.ZERO
+
+	# Kontrol Gerakan
 	if Input.is_action_pressed("move-up"):
 		$AnimatedSprite2D.play("up")
 		direction.y -= 1
@@ -18,23 +23,45 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.play("left")
 		direction.x -= 1
 	elif Input.is_action_pressed("action"):
-		var cashierSpots = get_parent().get_node("CashierSpots")
-		if cashierSpots.bodyEntered and Input.is_action_pressed("action"):
-			print("Ditekan")
+		var CashierSpots = get_parent().get_node("CashierSpots")
+		if CashierSpots.bodyEntered:
 			$AnimatedSprite2D.play("working")
-		if Dirt.bodyMasuk and Input.is_action_pressed("action"):
+
+		if not is_cleaning and current_dirt and current_dirt.bodyMasuk:
+			is_cleaning = true
 			$AnimatedSprite2D.play("working")
+			print("Pembersihan dimulai...")
+		elif is_cleaning:
+			bersihkan += 1
+			if bersihkan >= bersihkanDur:
+				current_dirt.bersihkanDone = true
+				is_cleaning = false
+				bersihkan = 0
+				print("Pembersihan selesai!")
 	else:
 		$AnimatedSprite2D.stop()
-	
-	
-	
+
+	# Hentikan proses jika tombol dilepas
+	if Input.is_action_just_released("action"):
+		if is_cleaning:
+			is_cleaning = false
+			bersihkan = 0
+			print("Pembersihan dibatalkan.")
+
+	# Kontrol Sprint
 	if Input.is_action_pressed("sprint"):
 		SPEED = 220.0
 	else:
 		SPEED = 150.0
-	
-	
-	
+
+	# Update kecepatan pemain
 	velocity = direction * SPEED
 	move_and_slide()
+
+# Tambahkan metode untuk menangani enter/exit dirt
+func on_enter_dirt(dirt):
+	current_dirt = dirt
+
+func on_exit_dirt(dirt):
+	if current_dirt == dirt:
+		current_dirt = null
