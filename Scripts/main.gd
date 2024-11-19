0469ee90ad82daacd110134f8fedc84fc4e4e217
birugh <notblue.https@gmail.com>
2024-11-19 23:35:19 +0700

    bug gamau ganti char

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,8 +5,20 @@ extends Node
 @export var map : PackedScene
 @export var scene: PackedScene
 
+#func _on_option_button_item_selected(index: int) -> void:
+	#match index:
+		#0: 
+			#player = preload("res://player/player.tscn")
+			#print("ob")
+		#1: 
+			#player = preload("res://player/stocker.tscn")
+			#print("stocker")
+	#pass # Replace with function body.
+
 
 func _ready() -> void:
+	print(player)
+	#player = load("res://player/player.tscn")
 	var upnp = UPNP.new()
 	upnp.discover()
 	upnp.add_port_mapping(9999)
@@ -57,9 +69,19 @@ func add_player(id):
 	var player_instance = player.instantiate()
 	player_instance.name = str(id)
 	%SpawnArea.add_child(player_instance)
+	player = null
 
 
 @rpc("any_peer")
 func remove_player(id):
 	if %SpawnArea.get_node(str(id)):
 		%SpawnArea.get_node(str(id)).queue_free()
+
+
+func _on_button_pressed() -> void:
+	player = load("res://player/player.tscn")
+	print(player)
+
+func _on_button_2_pressed() -> void:
+	player = preload("res://player/stocker.tscn")
+	print(player)
