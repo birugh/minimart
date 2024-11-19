d054f6cfbe57727e6fecb5b35e01f422e616a606
biru <151170580+birugh@users.noreply.github.com>
2024-11-19 19:41:42 +0700

    merging my new alternative multiplayer features

---
+++ Scripts/main.gd
@@ -0,0 +1,65 @@
+extends Node
+
+
+@export var player : PackedScene
+@export var map : PackedScene
+@export var scene: PackedScene
+
+
+func _ready() -> void:
+	var upnp = UPNP.new()
+	upnp.discover()
+	upnp.add_port_mapping(9999)
+	%PublicIP.text = upnp.query_external_address()
+
+
+func _on_host_button_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(9999)
+	multiplayer.multiplayer_peer = peer
+
+	multiplayer.peer_disconnected.connect(remove_player)
+
+	%Server.show()
+
+	load_game()
+
+
+func _on_join_button_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(%To.text, 9999)
+	multiplayer.multiplayer_peer = peer
+
+	multiplayer.connected_to_server.connect(load_game)
+	multiplayer.server_disconnected.connect(connection_lost)
+
+
+func _on_to_text_submitted(new_text: String) -> void:
+	_on_join_button_pressed() 
+
+
+func load_game():
+	%Menu.hide()
+	%MapInstance.add_child(map.instantiate())
+	add_player.rpc(multiplayer.get_unique_id())
+	%Content.add_child(scene.instantiate())
+	
+
+func connection_lost():
+	%Menu.show()
+
+	if %MapInstance.get_child(0):
+		%MapInstance.get_child(0).queue_free()
+
+
+@rpc("any_peer")
+func add_player(id):
+	var player_instance = player.instantiate()
+	player_instance.name = str(id)
+	%SpawnArea.add_child(player_instance)
+
+
+@rpc("any_peer")
+func remove_player(id):
+	if %SpawnArea.get_node(str(id)):
+		%SpawnArea.get_node(str(id)).queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,38 +1,23 @@
 extends CharacterBody2D
 
-var SPEED = 0
 
-var syncPos = Vector2(0,0)
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
 
 func _ready() -> void:
-	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
+	set_multiplayer_authority(name.to_int())
+	%DisplayAuthority.visible = is_multiplayer_authority()
+
 
 func _physics_process(delta: float) -> void:
-	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
-		var direction = Vector2.ZERO
-		if Input.is_action_pressed("move-up"):
-			$AnimatedSprite2D.play("up")
-			direction.y -= 1
-			print(direction)
-		elif Input.is_action_pressed("move-down"):
-			$AnimatedSprite2D.play("down")
-			direction.y += 1
-		elif Input.is_action_pressed("move-right"):
-			$AnimatedSprite2D.play('right')
-			direction.x += 1
-		elif Input.is_action_pressed("move-left"):
-			$AnimatedSprite2D.play("left")
-			direction.x -= 1
-		else:
-			$AnimatedSprite2D.stop()
-			
-		if Input.is_action_pressed("sprint"):
-			SPEED = 220.0
-		else:
-			SPEED = 150.0
-		
-		direction = direction.normalized()
-		velocity = direction * SPEED 
-		move_and_slide()
-	#else:
-		#global_position = global_position.lerp(syncPos, .5)
+	if not is_multiplayer_authority(): return
+
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
